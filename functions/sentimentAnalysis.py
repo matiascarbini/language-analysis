c3b383b52591283ec6bcfc83cae985a9e140d7d5
Matias Carbini <matias.carbini@gmail.com>
2022-01-20 15:42:01 -0300

    ajustes

--- functions/sentimentAnalysis.py
+++ functions/sentimentAnalysis.py
@@ -1,10 +1,6 @@
 from pysentimiento import create_analyzer
 analyzer = create_analyzer(task="sentiment", lang="es")
 
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-tokenizer = AutoTokenizer.from_pretrained("pysentimiento/robertuito-sentiment-analysis")
-model = AutoModelForSequenceClassification.from_pretrained("pysentimiento/robertuito-sentiment-analysis")
-
 from flask import Blueprint, jsonify, request
 
 sentimentAnalysis_api = Blueprint('sentimentAnalysis_api', __name__)
@@ -12,7 +8,8 @@ sentimentAnalysis_api = Blueprint('sentimentAnalysis_api', __name__)
 @sentimentAnalysis_api.route('/sentiment-analysis', methods=["POST"])
 def sentimentAnalysis():      
   text = request.json["text"]  
-  result = analyzer.predict(text)    
+  result = analyzer.predict(text)  
+    
   return jsonify({
     "sentimiento": result.output,
     "probabilidades": {
